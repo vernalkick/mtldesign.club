25f24a6673576585d8dc2584cc92d232a0d69a47
Kevin Clark <kevin@kevinclark.ca>
2018-10-31 22:20:30 -0400

    Improvements

--- src/components/event.js
+++ src/components/event.js
@@ -4,7 +4,7 @@ import moment from 'moment'
 export default ({ event, type, children }) => (
   <section className="event">
     <header className="event__header">
-      <span class="event__number">{type}</span>
+      <span className="event__number">{type}</span>
       <h2 className="event__date">{moment.utc(event.date).format("MMMM Do")}</h2>
       <span className="event__details">
         {event.time} @ <a href={event.location.url}>{event.location.name}</a> <a href={event.rsvp} className="event__rsvp external-link">RSVP</a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,35 @@ import Event from '../components/event'
 import Layout from '../components/layout'
 import moment from 'moment'
 
-const IndexPage = ({ data: {allEventsYaml, allWorkshopsYaml} }) => (
-  <Layout>
-    {allEventsYaml.edges.map(edge =>
-//      !moment(edge.node.date).isBefore() &&
-      // moment(edge.node.date).isBefore('2010-10-21') != 'November 1st' &&
-      <Event event={edge.node} type="Event" key={edge.node}>
-        <Meetup event={edge.node} />
-      </Event>
-    )}
-    {allWorkshopsYaml.edges.map(edge =>
-      <Event event={edge.node} type="Workshop" key={edge.node}>
-        <Workshop workshop={edge.node} />
-      </Event>
-    )}
-  </Layout>
-)
+const IndexPage = ({ data: {allEventsYaml, allWorkshopsYaml} }) => {
+  const allItems = [...allEventsYaml.edges, ...allWorkshopsYaml.edges]
+  const sortedItems = allItems.sort((a, b) => {
+    
+  })
+//  const sortedItems = allItems.sort(function (a, b) -> number {
+//    compare
+//  })
+  
+  console.log(sortedItems)
+  
+  return (
+    <Layout>
+    
+      {allItems.map(edge =>
+        edge.node.speakers ? (
+          <Event event={edge.node} type="Event" key={edge.node}>
+            <Meetup event={edge.node} />
+          </Event>
+        ) : (
+          <Event event={edge.node} type="Workshop" key={edge.node}>
+            <Workshop workshop={edge.node} />
+          </Event>
+        )
+      )}
+    
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
