17f99aff92b96876ff581df87dffc9f2496c5eeb
Kevin Clark <kevin@kevinclark.ca>
2018-11-04 19:06:25 -0500

    Add code of conduct back

---
+++ src/pages/code-of-conduct.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <div className="editorial">
+      <h1>Code of conduct</h1>
+
+      <p>Montréal Design Club is an event series aiming to bring the community together and everyone is encouraged to attend. The goal of the event series is to provide a safe, positive, and inclusive environment for participants and speakers to have meaningful conversations and learn from each other. In order to create this kind of safe, positive, and inclusive environment, we ask that all attendees follow the conduct guidelines outlined below.</p>
+
+      <p>These events should create a rewarding experience for any attendee &mdash; regardless of religion, ethnicity, physical appearance, body size, or any other characteristics or beliefs. All attendees are expected to conduct themselves in a way that that is respectful and inclusive of all individuals.</p>
+
+      <p>We ask that all Montréal Design Club attendees and speakers be respectful, inclusive, and aware of your words and actions/reactions. Be mindful of how your words or actions might affect others. Let’s all work together to create an environment that makes space for each other’s ideas and perspectives.</p>
+
+      <p>We encourage all participants to have open and honest discussions and to be responsible for their words and actions. If you observe someone behaving disrespectfully or insensitively towards someone else, (and if you feel comfortable engaging them) we encourage you to respectfully ask them to stop. If the person does not respond to your request, or you would prefer not to approach them, please contact a member of the organization team immediately. We’re here to listen and to help resolve any issues that may arise.</p>
+
+      <p>If you catch yourself being disrespectful or insensitive, or if you are approached by someone about your words or behavior, we encourage you to pause, listen openly to the concern, own what you’ve said or done, apologize, and move forward in a constructive and respectful way.</p>
+
+      <p>If any of these terms are violated, the participant will be asked to stop and must comply immediately. If an individual does not comply, organizers will take action as deemed appropriate, including expulsion from the events.</p>
+
+      <p>If you believe someone is violating the code of conduct please contact us by emailing <a href="mailto:kevin@kevinclark.ca" class="external-link">kevin@kevinclark.ca</a> All reports will be kept anonymous. If you believe someone is in physical danger, please notify local law enforcement immediately.</p>
+
+      <p>The intention of this guide is to make sure everyone is aware of the community expectations regarding how we conduct ourselves. If you have questions or would like further clarification on any of the points, please contact: <a href="mailto:kevin@kevinclark.ca" class="external-link">kevin@kevinclark.ca</a></p>
+
+      <p>We look forward to seeing you at the events!</p>
+    </div>
+  </Layout>
+)
