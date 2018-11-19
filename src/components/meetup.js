a96505101d9cd719e1a6299ba87b6856a22427de
Kevin Clark <kevin@kevinclark.ca>
2018-11-19 16:47:22 -0500

    Make images sorta work

--- src/components/meetup.js
+++ src/components/meetup.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { withPrefix } from 'gatsby'
 
 export default ({ event }) => (
   <main className="event__speakers">
     {event.speakers && event.speakers.map(speaker =>
       <div className="speaker" key={speaker.name}>
         <figure className="speaker__figure">
-          <img src={'/images/' + speaker.image} className="speaker__image" alt={speaker.name + "'s picture"} />
+          <img src={withPrefix('/images/' + speaker.image)} className="speaker__image" alt={speaker.name + "'s picture"} />
         </figure>
         <div className="speaker__info">
           <strong className="speaker__name">{speaker.name}</strong>
