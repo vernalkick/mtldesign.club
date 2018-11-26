6857dd6d09d7ccbf0e1b97460d10066b064fc79c
Kevin Clark <kevin@kevinclark.ca>
2018-11-26 09:12:21 -0500

    reduce parallax intensity

--- src/components/photobook.js
+++ src/components/photobook.js
@@ -9,7 +9,7 @@ export default ({ images }) => (
   <ParallaxProvider>
     <div className="photobook">
       {images.map((url, index) =>
-        <Parallax offsetYMax={20 * Math.floor(Math.random() * 6) + 1 } offsetYMin={-20} slowerScrollRate tag="div">
+        <Parallax offsetYMax={20 * Math.floor(Math.random() * 3) + 1 } offsetYMin={-20} slowerScrollRate tag="div">
           <div className={"photoframe " + "photoframe--" + index}>
             <img src={url} className={"photoframe__image " + "photoframe__image--" + index} />
           </div>
