45a24833cd533a60c2165685af67c97b1fbadaaa
Kevin Clark <kevin@kevinclark.ca>
2019-06-02 14:51:43 -0400

    Add CMS

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,45 +8,62 @@ import Footer from './footer'
 import '../assets/styles.css'
 import Banner from '../assets/images/banner.png'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+class Layout extends React.Component {
+  componentDidMount() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          })
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { property: 'og:title', content: 'Montréal Design Club' },
-            { property: 'og:description', content: 'A monthly event series highlighting some of the best designers in Montréal.' },
-            { property: 'og:image', content: `${Banner}`},
-            { name: 'description', content: 'A monthly event series highlighting some of the best designers of Montreal.'},
-            { name: 'author', content: 'Kevin Clark'}
-          ]}
-        >
-          <html lang="en" />
-          <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet" />
-        </Helmet>
-        <div className="container">
-          <div className="wrap">
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <main>
-              {children}
-            </main>
-            <Footer />
-          </div>
-        </div>
-      </>
-    )}
-  />
-)
+      })
+    }
+  }
+
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
+        `}
+        render={data => (
+          <>
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { property: 'og:title', content: 'Montréal Design Club' },
+                { property: 'og:description', content: 'A monthly event series highlighting some of the best designers in Montréal.' },
+                { property: 'og:image', content: `${Banner}`},
+                { name: 'description', content: 'A monthly event series highlighting some of the best designers of Montreal.'},
+                { name: 'author', content: 'Kevin Clark'}
+              ]}
+            >
+              <html lang="en" />
+              <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet" />
+              <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            </Helmet>
+            <div className="container">
+              <div className="wrap">
+                <Header siteTitle={data.site.siteMetadata.title} />
+                <main>
+                  {this.props.children}
+                </main>
+                <Footer />
+              </div>
+            </div>
+          </>
+        )}
+      />
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
