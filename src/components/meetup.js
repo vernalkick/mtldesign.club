299974ad0604c4e62c055d305cb3814de1f8d73c
Kevin Clark <kevin@kevinclark.ca>
2019-06-02 17:25:00 -0400

    Attempt to fix weird image format

--- src/components/meetup.js
+++ src/components/meetup.js
@@ -1,20 +1,30 @@
 import React from 'react'
 
-export default ({ event }) => (
-  <main className="event__speakers">
-    {event.speakers && event.speakers.map(speaker =>
-      <div className="speaker" key={speaker.name}>
-        <figure className="speaker__figure">
-          <img src={speaker.image && speaker.image.publicURL && speaker.image.publicURL} className="speaker__image" alt={speaker.name + "'s picture"} />
-        </figure>
-        <div className="speaker__info">
-          <strong className="speaker__name">{speaker.name}</strong>
-          <span className="speaker__description">{speaker.title}</span>
-          {speaker.language &&
-            <span className="speaker__language">{speaker.language}</span>
-          }
+export default ({ event }) => {
+  String.prototype.sanitize = function() {
+    var s = this
+    if (s.charAt(0) == '/') {
+      s = s.substr(1)
+    }
+    return s
+  }
+
+  return (
+    <main className="event__speakers">
+      {event.speakers && event.speakers.map(speaker =>
+        <div className="speaker" key={speaker.name}>
+          <figure className="speaker__figure">
+            <img src={speaker.image && speaker.image.publicURL && speaker.image.publicURL.sanitize()} className="speaker__image" alt={speaker.name + "'s picture"} />
+          </figure>
+          <div className="speaker__info">
+            <strong className="speaker__name">{speaker.name}</strong>
+            <span className="speaker__description">{speaker.title}</span>
+            {speaker.language &&
+              <span className="speaker__language">{speaker.language}</span>
+            }
+          </div>
         </div>
-      </div>
-    )}
-  </main>
-)
+      )}
+    </main>
+  )
+}
