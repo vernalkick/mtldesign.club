c44ab15f37ed535cbc35d555b1bc8acb516d812d
Kevin Clark <kevin@kevinclark.ca>
2019-08-09 21:26:18 -0400

    Remove extra image

--- src/components/photobook.js
+++ src/components/photobook.js
@@ -6,7 +6,6 @@ export default ({ images }) => (
     {images.map((edge, index) =>
       <div className={"photoframe " + "photoframe--" + index}>
         <Img fluid={edge.node.childImageSharp.fluid} className={"photoframe__image"} />
-        <img src={edge.node.publicURL}  />
       </div>
     )}
   </div>
